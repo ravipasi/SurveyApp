3aaab62e084221694ac71f94add5efd037031f53
ravipasi <ravipasi1@gmail.com>
2025-12-10 17:41:40 +0530

    v1-Update

--- Util/DBConnection.cs
+++ Util/DBConnection.cs
@@ -4,6 +4,7 @@
     {
         // 🔐 Hardcoded string — suitable for isolated environments
          private static readonly string cs = "Server=182.19.5.165;Database=VLDev;UID=adminrole;Password=@dminr0le;Connect Timeout=360000;TrustServerCertificate=True";
+        //private static readonly string cs = "Server=182.19.5.165;Database=VLDev;UID=adminrole;Password=@dminr0le;Connect Timeout=360000;TrustServerCertificate=True";
         //private static readonly string cs = "Server=(Local);Database=VLDev;Integrated Security=True;Connect Timeout=360000;TrustServerCertificate=True";
         public static string ConnectionString => cs;
     }
